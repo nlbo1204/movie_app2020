373bd99cf33ec3228442af12c1cc7de9d585c38d
nlbo1204 <nlbo1204@gmail.com>
2020-10-20 22:00:20 +0900

    Updates

---
+++ precache-manifest.047796953151ee66d430c52167651593.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29fe7327f8327b002392fb00d94d847c",
+    "url": "/movie_app2020/index.html"
+  },
+  {
+    "revision": "872e37ccae7d8324b36d",
+    "url": "/movie_app2020/static/css/main.7f33008b.chunk.css"
+  },
+  {
+    "revision": "e3b63d7ac993dddafe2b",
+    "url": "/movie_app2020/static/js/2.5946ca1b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app2020/static/js/2.5946ca1b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "872e37ccae7d8324b36d",
+    "url": "/movie_app2020/static/js/main.5bd81f51.chunk.js"
+  },
+  {
+    "revision": "0fd056db83622f72676d",
+    "url": "/movie_app2020/static/js/runtime-main.2e4fc247.js"
+  }
+]);
